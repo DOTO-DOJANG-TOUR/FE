5ccbb04a63fc912c3598abf0ece414f0971db7f2
p1001q <qrwrer123@gmail.com>
2026-08-18 10:18:04 +0900

    💄 Design: Pretendard 폰트 및 theme.ts 공통 디자인 토큰 추가 (동기화 작업) (#12)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,20 @@
+import { useFonts } from 'expo-font';
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 export default function TabLayout() {
+  const [loaded] = useFonts({
+    PretendardRegular: require('../../assets/fonts/Pretendard-Regular.otf'),
+    PretendardMedium: require('../../assets/fonts/Pretendard-Medium.otf'),
+    PretendardSemiBold: require('../../assets/fonts/Pretendard-SemiBold.otf'),
+    PretendardBold: require('../../assets/fonts/Pretendard-Bold.otf'),
+  });
   const colorScheme = useColorScheme();
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }} />
