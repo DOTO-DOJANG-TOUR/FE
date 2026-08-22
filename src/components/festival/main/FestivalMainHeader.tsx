127ef9c1c7aa5e25a255e4735e9d8f012f762627
BB545 <black1016@hanmail.net>
2026-08-22 15:50:31 +0900

    ✨ Feat: festival main 화면 header 컴포넌트 구현 (#23)

---
+++ src/components/festival/main/FestivalMainHeader.tsx
@@ -0,0 +1,28 @@
+import TextLogo from '@/assets/images/festival/common/text-logo.png';
+import { SearchIcon } from '@/components/icons/SearchIcon';
+import { Image, StyleSheet, View } from 'react-native';
+
+export default function FestivalMainHeader() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={TextLogo}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <SearchIcon />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  image: {
+    width: 55,
+    height: 16,
+  },
+});
