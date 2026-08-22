f22a62e8ceae3be4456c20bfe13843b30a5da55e
BB545 <black1016@hanmail.net>
2026-08-22 17:33:51 +0900

    ✨ Feat: festival main 화면 지역별 축제 카테고리 컴포넌트 구현 (#23)

---
+++ src/components/festival/main/RegionCategoryCard.tsx
@@ -0,0 +1,45 @@
+import DefaultCardImage from '@/assets/images/festival/common/card-dim-2.png';
+import { Colors, FontFamily, Radius } from '@/constants/theme';
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native';
+
+type Props = {
+    image?: ImageSourcePropType,
+    region: string,
+}
+
+export default function RegionCategoryCard({
+    image,
+    region
+}: Props) {
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={image ?? DefaultCardImage}
+                style={styles.image}
+                resizeMode="cover"
+            />
+            <Text style={styles.region}>{region}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 62,
+        height: 62,
+        borderRadius: Radius.md,
+        backgroundColor: '#D3D3D3',
+    },
+    region: {
+        paddingTop: 4,
+        fontFamily: FontFamily.medium,
+        fontSize: 13,
+        color: Colors.gray.gray70,
+        lineHeight: 13 * 1.5,
+    },
+});
