bf407173ad177f23326c1b2004c281aff2df6700
BB545 <black1016@hanmail.net>
2026-08-29 19:07:29 +0900

    ✨ Feat: 검색 페이지 라우팅 추가 (#27) app/search/festival.tsx - 축제 검색 페이지 app/search/tour.tsx - 관광지 검색 페이지 app/search/_layout.tsx - 검색 페이지 공통 네비게이션 설정

---
+++ src/app/search/festival.tsx
@@ -0,0 +1,5 @@
+import SearchPage from '@/pages/search/SearchPage';
+
+export default function FestivalSearch() {
+  return <SearchPage type="festival" />;
+}
---
+++ src/app/search/tour.tsx
@@ -0,0 +1,5 @@
+import SearchPage from '@/pages/search/SearchPage';
+
+export default function TourSearch() {
+  return <SearchPage type="tour" />;
+}
